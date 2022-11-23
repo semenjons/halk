cf6753acfe5fde3aca12df00c963343eed8c18a9
semen.jons <semen.jons@yandex.ru>
2022-11-23 21:07:12 +0300

    сохранил2

---
+++ nottil2/Program.cs
@@ -0,0 +1,27 @@
+﻿ //Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+using static System.Console; 
+Clear();
+
+WriteLine("Введите число, и ниже вы увидите третью цифру от вашего числа");
+int n = Convert.ToInt32(ReadLine()); 
+int nnomer(int d)
+        {
+            int result = -1; 
+            {
+                while (d > 999) 
+                {
+                    d = d / 10; 
+                }
+                result = d % 10; 
+            }
+            return result; 
+        }
+if (nnomer(n) == -1) 
+Console.WriteLine("Третьей цифры нет");
+else 
+Console.WriteLine($"Третья цифра это: {nnomer(n)}"); 
+
